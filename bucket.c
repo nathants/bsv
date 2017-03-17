ae61ac7313edb322a3b3aedbef1d58544047d079
nathants <me@nathants.com>
2017-03-16 21:41:21 -0700

    murmur3 consistent hashing to choose a bucket by the first column in a row

---
+++ bucket.c
@@ -0,0 +1,50 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "murmur3.h"
+
+#define MAX_COLUMNS 64
+#define MAX_LINE_BYTES 8192
+
+void showusage() {
+    fprintf(stderr, "\nMurmurHash3_x86_32 the first column, modulo the number of buckets,");
+    fprintf(stderr, "and insert the selected bucket as the new first column, offsetting the rest.\n");
+    fprintf(stderr, "\nusage: $ bucket DELIMETER NUM_BUCKETS\n");
+    exit(1);
+}
+
+static int empty(const char * s) {
+    while (*s != '\0') {
+        if (!isspace(*s))
+            return 0;
+        s++;
+    }
+    return 1;
+}
+
+int main(int argc, const char **argv) {
+    char delimiter[2], *line_ptr, *first_column, line[MAX_LINE_BYTES], hash_str[15];
+    uint32_t num_buckets, hash_num[1];
+    if (argc < 3)
+        showusage();
+    delimiter[0] = argv[1][0];
+    num_buckets = atoi(argv[2]);
+    while (fgets(line, sizeof(line), stdin)) {
+        if (empty(line)) {
+            fputs("\n", stdout);
+            continue;
+        }
+        /* if (strlen(line) == sizeof(line) - 1) { fprintf(stderr, "error: encountered a line longer than the max of %d chars\n", MAX_LINE_BYTES); exit(1); } // per line error checking */
+        line_ptr = line;
+        first_column = strsep(&line_ptr, ",");
+        MurmurHash3_x86_32(first_column, strlen(first_column), 0, hash_num);
+        sprintf(hash_str, "%d", hash_num[0] % num_buckets);
+        fputs(hash_str,     stdout);
+        fputs(delimiter,    stdout);
+        fputs(first_column, stdout);
+        fputs(delimiter,    stdout);
+        fputs(line_ptr,     stdout);
+    }
+    return 0;
+}
---
+++ bucket_test.py
@@ -0,0 +1,40 @@
+import pytest
+import string
+from hypothesis import given, settings
+from hypothesis.strategies import text, lists, composite, integers
+import shell
+import hashlib
+
+stdinpath = '/tmp/%s.stdin' % hashlib.md5(__file__.encode('ascii')).hexdigest()
+stdoutpath = '/tmp/%s.stdout' % hashlib.md5(__file__.encode('ascii')).hexdigest()
+
+def unindent(text):
+    return '\n'.join([x.lstrip() for x in text.splitlines()]) + '\n'
+
+def run(stdin, *args):
+    with open(stdinpath, 'w') as f:
+        f.write(unindent(stdin))
+    try:
+        shell.run(*(('cat', stdinpath, '|') + args + ('>', stdoutpath)), stream=True)
+    except:
+        raise AssertionError from None
+    with open(stdoutpath) as f:
+        return f.read()
+
+shell.run('make bucket', stream=True)
+
+MAX_COLUMNS = 64
+MAX_LINE_BYTES = 8192
+
+def test_basic():
+    stdin = """
+    a,b,c,d
+    1,2,3
+    x,y
+    """
+    stdout = """
+    2,a,b,c,d
+    3,1,2,3
+    3,x,y
+    """
+    assert unindent(stdout) == run(stdin, './bucket , 4')
