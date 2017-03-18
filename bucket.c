aab718474a0ce9de5bdf0d821e525f0f3c0b6242
nathants <me@nathants.com>
2017-03-17 20:41:25 -0700

    tests and error handling

--- bucket.c
+++ bucket.c
@@ -1,4 +1,5 @@
 #include <ctype.h>
+#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -23,13 +24,27 @@ static int empty(const char * s) {
     return 1;
 }
 
+static int mod (int a, int b) {
+   int ret = a % b;
+   if(ret < 0)
+     ret+=b;
+   return ret;
+}
+
 int main(int argc, const char **argv) {
-    char delimiter[2], *line_ptr, *first_column, line[MAX_LINE_BYTES], hash_str[15];
-    uint32_t num_buckets, hash_num[1];
+    /* def and init */
+    char delimiter[2], *line_ptr, *first_column, line[MAX_LINE_BYTES], hash_str[156];
+    int num_buckets, hash_num[1];
+
+    /* parse argv */
     if (argc < 3)
         showusage();
     delimiter[0] = argv[1][0];
+    if (strlen(argv[2]) > 8) { fprintf(stderr, "NUM_BUCKETS must be less than 1e8, got: %s\n", argv[2]); exit(1); }
     num_buckets = atoi(argv[2]);
+    if (num_buckets < 1) { fprintf(stderr, "NUM_BUCKETS must be positive, got: %d\n", num_buckets); exit(1); }
+
+    /* do the work */
     while (fgets(line, sizeof(line), stdin)) {
         if (empty(line)) {
             fputs("\n", stdout);
@@ -37,15 +52,20 @@ int main(int argc, const char **argv) {
         }
         if (strlen(line) == sizeof(line) - 1) { fprintf(stderr, "error: encountered a line longer than the max of %d chars\n", MAX_LINE_BYTES); exit(1); }
         line_ptr = line;
+        line_ptr = strsep (&line_ptr, "\n");
         first_column = strsep(&line_ptr, ",");
-        if (line_ptr == NULL) { fprintf(stderr, "error: input line with single column: %s", first_column); exit(1); }
         MurmurHash3_x86_32(first_column, strlen(first_column), 0, hash_num);
-        sprintf(hash_str, "%d", hash_num[0] % num_buckets);
-        fputs(hash_str,     stdout);
-        fputs(delimiter,    stdout);
+        sprintf(hash_str, "%d", mod(hash_num[0], num_buckets));
+        fputs(hash_str, stdout);
+        fputs(delimiter, stdout);
         fputs(first_column, stdout);
-        fputs(delimiter,    stdout);
-        fputs(line_ptr,     stdout);
+        if (line_ptr != NULL) {
+            fputs(delimiter, stdout);
+            fputs(line_ptr, stdout);
+        }
+        fputs("\n",     stdout);
     }
+
+    /* all done */
     return 0;
 }
--- bucket_test.py
+++ bucket_test.py
@@ -1,8 +1,9 @@
 import pytest
+import murmur3 # https://github.com/nathants/murmur3/tree/master/python
 import string
 from hypothesis import given, settings
 from hypothesis.strategies import text, lists, composite, integers
-import shell
+import shell # https://github.com/nathants/py-shell
 import hashlib
 
 stdinpath = '/tmp/%s.stdin' % hashlib.md5(__file__.encode('ascii')).hexdigest()
@@ -23,28 +24,73 @@ def run(stdin, *args):
 
 shell.run('make bucket', stream=True)
 
-MAX_COLUMNS = 64
 MAX_LINE_BYTES = 8192
 
-# def test_basic():
-#     stdin = """
-#     a,b,c,d
-#     1,2,3
-#     x,y
-#     """
-#     stdout = """
-#     2,a,b,c,d
-#     3,1,2,3
-#     3,x,y
-#     """
-#     assert unindent(stdout) == run(stdin, './bucket , 4')
+@composite
+def inputs(draw):
+    num_columns = draw(integers(min_value=1, max_value=12))
+    column = text(string.ascii_lowercase)
+    line = lists(column, min_size=num_columns, max_size=num_columns)
+    lines = draw(lists(line, min_size=3))
+    csv = '\n'.join([','.join(x) for x in lines]) + '\n'
+    buckets = draw(integers(min_value=1, max_value=1e8 - 1))
+    return (buckets, csv)
 
-def test_basic2():
+def expected(buckets, csv):
+    xs = ["%d,%s" % (murmur3.hash(x.split(',')[0]) % buckets, x)
+          if x.strip()
+          else x
+          for x in csv.splitlines()]
+
+    return '\n'.join(xs) + '\n'
+
+@given(inputs())
+@settings(max_examples=100)
+def test_props(args):
+    buckets, csv = args
+    result = expected(buckets, csv)
+    assert result == run(csv, './bucket ,', buckets)
+
+def test_single_column():
+    stdin = """
+    a
+    1
+    x
+    """
+    stdout = """
+    2,a
+    3,1
+    3,x
+    """
+    assert unindent(stdout) == run(stdin, './bucket , 4')
+
+def test_basic():
     stdin = """
     a,b,c,d
-    1
+    1,2,3
     x,y
     """
     stdout = """
+    2,a,b,c,d
+    3,1,2,3
+    3,x,y
     """
     assert unindent(stdout) == run(stdin, './bucket , 4')
+
+def test_fails_when_non_positive_buckets():
+    stdin = 'a'
+    res = shell.run('./bucket , 0 2>&1', stdin=stdin, warn=True)
+    assert res['exitcode'] == 1
+    assert 'NUM_BUCKETS must be positive, got: 0' == res['output']
+
+def test_fails_when_too_many_buckets():
+    stdin = 'a'
+    res = shell.run('./bucket ,', int(1e8), '2>&1', stdin=stdin, warn=True)
+    assert res['exitcode'] == 1
+    assert 'NUM_BUCKETS must be less than 1e8, got: 100000000' == res['output']
+
+def test_fails_when_lines_too_long():
+    stdin = 'a' * MAX_LINE_BYTES
+    res = shell.run('./bucket , 4 2>&1', stdin=stdin, warn=True)
+    assert res['exitcode'] == 1
+    assert 'error: encountered a line longer than the max of 8192 chars' == res['output']
