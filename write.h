6b7634739706ca6a44fee90eab365fadc45257f5
nathants <me@nathants.com>
2017-08-09 21:08:46 -0700

    check writes for error

--- write.h
+++ write.h
@@ -8,21 +8,27 @@
     char *write_buffer = malloc(WRITE_BUFFER_SIZE); \
     int write_offset = 0;
 
-#define WRITE(str, size)                                                        \
-    do {                                                                        \
-        if (size > WRITE_BUFFER_SIZE - write_offset) {                          \
-            fwrite_unlocked(write_buffer, sizeof(char), write_offset, stdout);  \
-            memcpy(write_buffer, str, size);                                    \
-            write_offset = size;                                                \
-        } else {                                                                \
-            memcpy(write_buffer + write_offset, str, size);                     \
-            write_offset += size;                                               \
-        }                                                                       \
+#define WRITE(str, size)                                                                                \
+    do {                                                                                                \
+        if (size > WRITE_BUFFER_SIZE - write_offset) {                                                  \
+            if (write_offset != fwrite_unlocked(write_buffer, sizeof(char), write_offset, stdout)) {    \
+                fprintf(stderr, "error: failed to write output");                                       \
+                exit(1);                                                                                \
+            }                                                                                           \
+            memcpy(write_buffer, str, size);                                                            \
+            write_offset = size;                                                                        \
+        } else {                                                                                        \
+            memcpy(write_buffer + write_offset, str, size);                                             \
+            write_offset += size;                                                                       \
+        }                                                                                               \
     } while (0)
 
-#define WRITE_FLUSH() \
-    do {                                                            \
-        fwrite(write_buffer, sizeof(char), write_offset, stdout);   \
+#define WRITE_FLUSH()                                                                   \
+    do {                                                                                \
+        if (write_offset != fwrite(write_buffer, sizeof(char), write_offset, stdout)) { \
+            fprintf(stderr, "error: failed to write output");                           \
+            exit(1);                                                                    \
+        }                                                                               \
     } while (0)
 
 #endif
--- writes.h
+++ writes.h
@@ -13,22 +13,31 @@
         writes_offset[i] = 0;                           \
     }
 
-#define WRITES(str, size, file_num)                                                                                     \
-    do {                                                                                                                \
-        if (size > WRITES_BUFFER_SIZE - writes_offset[file_num]) {                                                      \
-            fwrite_unlocked(writes_buffers[file_num], sizeof(char), writes_offset[file_num], writes_files[file_num]);   \
-            memcpy(writes_buffers[file_num], str, size);                                                                \
-            writes_offset[file_num] = size;                                                                             \
-        } else {                                                                                                        \
-            memcpy(writes_buffers[file_num] + writes_offset[file_num], str, size);                                      \
-            writes_offset[file_num] += size;                                                                            \
-        }                                                                                                               \
+#define WRITES(str, size, file_num)                                                     \
+    do {                                                                                \
+        if (size > WRITES_BUFFER_SIZE - writes_offset[file_num]) {                      \
+            if (writes_offset[file_num] != fwrite_unlocked(writes_buffers[file_num],    \
+                                                           sizeof(char),                \
+                                                           writes_offset[file_num],     \
+                                                           writes_files[file_num])) {   \
+                fprintf(stderr, "error: failed to write output");                       \
+                exit(1);                                                                \
+            }                                                                           \
+            memcpy(writes_buffers[file_num], str, size);                                \
+            writes_offset[file_num] = size;                                             \
+        } else {                                                                        \
+            memcpy(writes_buffers[file_num] + writes_offset[file_num], str, size);      \
+            writes_offset[file_num] += size;                                            \
+        }                                                                               \
     } while (0)
 
-#define WRITES_FLUSH(num_files)                                                         \
-    do {                                                                                \
-        for (int i = 0; i < num_files; i++)                                             \
-            fwrite(writes_buffers[i], sizeof(char), writes_offset[i], writes_files[i]); \
+#define WRITES_FLUSH(num_files)                                                                                     \
+    do {                                                                                                            \
+        for (int i = 0; i < num_files; i++)                                                                         \
+            if (writes_offset[i] != fwrite(writes_buffers[i], sizeof(char), writes_offset[i], writes_files[i])) {    \
+                fprintf(stderr, "error: failed to write output");                                                   \
+                exit(1);                                                                                            \
+            }                                                                                                       \
     } while(0)
 
 
