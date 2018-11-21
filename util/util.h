1b6d578f26017bcbe336b7258af7fe25bb9a73f1
nathants <me@nathants.com>
2018-11-20 23:39:05 -0800

    formatting

--- util/util.h
+++ util/util.h
@@ -42,14 +42,10 @@
             if (_csv_char_index - _csv_offset == _csv_bytes_read) {                                                     \
                 /* figure out how many bytes to move */                                                                 \
                 _csv_offset = 0;                                                                                        \
-                for (_csv_i = 0; _csv_i <= csv_max; _csv_i++) {                                                         \
+                for (_csv_i = 0; _csv_i <= csv_max; _csv_i++)                                                           \
                     _csv_offset += csv_sizes[_csv_i] + 1;                                                               \
-                }                                                                                                       \
                 _csv_offset--;                                                                                          \
-                if (_csv_offset >= BUFFER_SIZE) {                                                                       \
-                    fprintf(stderr, "error: line longer than BUFFER_SIZE\n");                                           \
-                    exit(1);                                                                                            \
-                }                                                                                                       \
+                if (_csv_offset >= BUFFER_SIZE) { fprintf(stderr, "error: line longer than BUFFER_SIZE\n"); exit(1); }  \
                 /* move the bytes to head of buffer, and update vars for new buffer positions */                        \
                 memmove(_csv_buffer, csv_columns[0], _csv_offset);                                                      \
                 _csv_escaped = _csv_buffer[_csv_offset - 1] == '\\' ;                                                   \
@@ -58,10 +54,7 @@
                     csv_columns[_csv_i] = csv_columns[_csv_i - 1] + csv_sizes[_csv_i - 1] + 1;                          \
                 /* read into the buffer */                                                                              \
                 _csv_char_index = _csv_offset;                                                                          \
-                _csv_bytes_read = fread_unlocked(_csv_buffer + _csv_offset,                                             \
-                                                 1,                                                                     \
-                                                 BUFFER_SIZE - _csv_offset,                                             \
-                                                 file);                                                                 \
+                _csv_bytes_read = fread_unlocked(_csv_buffer + _csv_offset, 1, BUFFER_SIZE - _csv_offset, file);        \
             }                                                                                                           \
             /* process buffer char by char */                                                                           \
             if (_csv_char_index - _csv_offset != _csv_bytes_read) {                                                     \
@@ -69,33 +62,25 @@
                 while (_csv_char_index - _csv_offset != _csv_bytes_read) {                                              \
                     _csv_char = _csv_buffer[_csv_char_index];                                                           \
                     /* start next column */                                                                             \
-                    if (_csv_char == CSV_DELIMITER &&                                                                   \
-                        (_csv_char_index == 0 || _csv_buffer[_csv_char_index - 1] != '\\'))                             \
-                    {                                                                                                   \
-                        if (++csv_max >= CSV_MAX_COLUMNS) {                                                             \
-                            fprintf(stderr, "error: line with more than %d columns\n", CSV_MAX_COLUMNS);                \
-                            exit(1);                                                                                    \
-                        }                                                                                               \
+                    if (_csv_char == CSV_DELIMITER && !(_csv_escaped || _csv_buffer[_csv_char_index - 1] == '\\')) {    \
+                        if (++csv_max >= CSV_MAX_COLUMNS) { fprintf(stderr, "error: line with more than %d columns\n", CSV_MAX_COLUMNS); exit(1); } \
                         csv_sizes[csv_max] = 0;                                                                         \
                         csv_columns[csv_max] = _csv_buffer + _csv_char_index + 1;                                       \
-                    }                                                                                                   \
-                    /* sanely handle null bytes in input */                                                             \
-                    else if (_csv_char == '\0') {                                                                       \
+                        /* sanely handle null bytes in input */                                                         \
+                    } else if (_csv_char == '\0') {                                                                     \
                         _csv_buffer[_csv_char_index] = ' ';                                                             \
                         csv_sizes[csv_max]++;                                                                           \
-                    }                                                                                                   \
-                    /* line is ready. prepare updates for the next iteration, and return control to caller */           \
-                    else if (_csv_char == '\n')                                                                         \
-                    {                                                                                                   \
+                        /* line is ready. prepare updates for the next iteration, and return control to caller */       \
+                    } else if (_csv_char == '\n') {                                                                     \
                         _csv_update_columns = 1;                                                                        \
                         _csv_next_column[0] = _csv_buffer + _csv_char_index + 1;                                        \
                         _csv_char_index++;                                                                              \
                         _csv_handled = 1;                                                                               \
                         _csv_break = 1;                                                                                 \
                         break; /* break out of double while loop */                                                     \
-                    }                                                                                                   \
-                    /* normal character increases current column size */                                                \
-                    else csv_sizes[csv_max]++;                                                                          \
+                        /* normal character increases current column size */                                            \
+                    } else                                                                                              \
+                        csv_sizes[csv_max]++;                                                                           \
                     /* bump the char index */                                                                           \
                     _csv_char_index++;                                                                                  \
                 }                                                                                                       \
@@ -108,9 +93,7 @@
                 else                                                                                                    \
                     /* the last row didnt have a newline, so lets return control to the caller, and stop next time */   \
                     _csv_handled = 1;                                                                                   \
-                if (feof(file))                                                                                         \
-                    ;                                                                                                   \
-                else if (ferror(file)) {                                                                                \
+                if (ferror(file)) {                                                                                     \
                     fprintf(stderr, "error: couldnt read input\n");                                                     \
                     exit(1);                                                                                            \
                 }                                                                                                       \
@@ -128,8 +111,8 @@
 
 #define DUMP(file, max, columns, sizes)                                                                                 \
     do {                                                                                                                \
-        if (max > CSV_MAX_COLUMNS) { fprintf(stderr, "error: cannot have more then 2**16 columns"); exit(1); }                   \
-        _dump_ushort = (unsigned short)max;                                                                            \
+        if (max > CSV_MAX_COLUMNS) { fprintf(stderr, "error: cannot have more then 2**16 columns"); exit(1); }          \
+        _dump_ushort = (unsigned short)max;                                                                             \
         WRITE(&_dump_ushort, 2, file);                                                                                  \
         for (_dump_i = 0; _dump_i <= max; _dump_i++) {                                                                  \
             if (sizes[_dump_i] > CSV_MAX_COLUMNS) { fprintf(stderr, "error: cannot have columns with more than 2**16 bytes, column: %d,size: %d, content: %.*s...", _dump_i, sizes[_dump_i], 10, columns[_dump_i]); exit(1); } \
@@ -145,11 +128,15 @@
 
 #define LOAD_INIT_VARS()                                                                        \
     READ_INIT_VARS();                                                                           \
-    int _load_bytes, load_stop, _load_sum, _load_i, load_max;                                   \
-    unsigned short _load_ushort;                                                                \
-    int load_sizes[CSV_MAX_COLUMNS];                                                            \
+    int _load_bytes;                                                                            \
+    int _load_sum;                                                                              \
+    int _load_i;                                                                                \
     char *_load_buffer = malloc(BUFFER_SIZE);                                                   \
     if (_load_buffer == NULL) { fprintf(stderr, "error: failed to allocate memory"); exit(1); } \
+    unsigned short _load_ushort;                                                                \
+    int load_max;                                                                               \
+    int load_stop;                                                                              \
+    int load_sizes[CSV_MAX_COLUMNS];                                                            \
     char *load_columns[CSV_MAX_COLUMNS];
 
 /* TODO is if more performant to merge LOAD and READ? */
@@ -197,17 +184,15 @@
         }                                                                                                           \
     } while (0)
 
-#define WRITE_FLUSH(file)                                                       \
-    do {                                                                        \
-        if (_write_offset != fwrite(_write_buffer, 1, _write_offset, file)) {   \
-            fprintf(stderr, "error: failed to write output");                   \
-            exit(1);                                                            \
-        }                                                                       \
+#define WRITE_FLUSH(file)                                                                                   \
+    do {                                                                                                    \
+        _write_bytes = fwrite(_write_buffer, 1, _write_offset, file);                                       \
+        if (_write_offset != _write_bytes) { fprintf(stderr, "error: failed to write output"); exit(1); }   \
     } while (0)
 
 #define READ_INIT_VARS()                                                                        \
-    char *read_buffer;                                                                          \
     int read_bytes;                                                                             \
+    char *read_buffer;                                                                          \
     char *_read_buffer = malloc(BUFFER_SIZE);                                                   \
     if (_read_buffer == NULL) { fprintf(stderr, "error: failed to allocate memory"); exit(1); } \
     int _read_bytes_left;                                                                       \
