cfc58dbff4251e9c2e192056e41fd9fdcee29f05
nathants <me@nathants.com>
2019-09-24 11:35:55 -0700

    use malloc helper

--- util/row.h
+++ util/row.h
@@ -13,21 +13,21 @@ typedef struct row_s {
     int _row_i;                                 \
     int _row_offset;
 
-#define ROW(_str, _max, _size, _sizes, _columns)                                                                        \
+#define ROW(_str, _max, _size, _sizes)                                                                                  \
     do {                                                                                                                \
-        row = malloc(sizeof(row_t)); if (row == NULL) { fprintf(stderr, "error: failed to allocate memory"); exit(1); } \
+        MALLOC(row, sizeof(row_t));                                                                                     \
         row->max = _max;                                                                                                \
         row->size = _size;                                                                                              \
-        row->buffer = malloc(_size + 1); if (row->buffer == NULL) { fprintf(stderr, "error: failed to allocate memory"); exit(1); } \
+        MALLOC(row->buffer, _size + 1);                                                                                 \
         memcpy(row->buffer, _str, _size);                                                                               \
         row->buffer[_size] = '\0';                                                                                      \
-        row->sizes = malloc(sizeof(int) * (_max + 1)); if (row->sizes == NULL) { fprintf(stderr, "error: failed to allocate memory"); exit(1); } \
+        MALLOC(row->sizes, sizeof(int) * (_max + 1))                                                                    \
         memcpy(row->sizes, _sizes, sizeof(int) * (_max + 1));                                                           \
-        row->columns = malloc(sizeof(char*) * (_max + 1));                                                              \
+        MALLOC(row->columns, sizeof(char*) * (_max + 1));                                                               \
         _row_offset = 0;                                                                                                \
-        for (_row_i = 0; _row_i <= row->max; _row_i++) {                                                                               \
-            row->columns[_row_i] = row->buffer + _row_offset;                                                                \
-            _row_offset += row->sizes[_row_i];                                                                               \
+        for (_row_i = 0; _row_i <= row->max; _row_i++) {                                                                \
+            row->columns[_row_i] = row->buffer + _row_offset;                                                           \
+            _row_offset += row->sizes[_row_i];                                                                          \
         }                                                                                                               \
     } while(0)
 
