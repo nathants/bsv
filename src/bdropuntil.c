9f9d6fb2ecdef145978f04d60d1f58d931d85b61
nathants <me@nathants.com>
2019-10-10 21:40:48 -0700

    comment bdropuntil

--- src/bdropuntil.c
+++ src/bdropuntil.c
@@ -15,27 +15,27 @@ int main(int argc, const char **argv) {
     MALLOC(value, BUFFER_SIZE);
     while (1) {
         LOAD(0);
-        if (load_stop) {
-            if (done_skipping) {
+        if (load_stop) { /* ---------------------------------------------- the last chunk and possibly need to backup to the previous chunk to find a match */
+            if (done_skipping) { /* -------------------------------------- already gone back to a previous chunk, time to stop */
                 break;
-            } else {
+            } else { /* -------------------------------------------------- go back and check the entire last chunk for a match */
                 READ_GOTO_LAST_CHUNK(0);
                 done_skipping = 1;
             }
-        } else {
-            if (matched) {
+        } else { /* ------------------------------------------------------ reading data chunk by chunk, checking the first row and the proceeding to the next chunk  */
+            if (matched) { /* -------------------------------------------- once a match is found dump every row */
                 DUMP(0, load_max, load_columns, load_sizes, load_size);
-            } else {
-                strncpy(value, load_columns[0], load_sizes[0]);
+            } else { /* -------------------------------------------------- check for a match */
+                memcpy(value, load_columns[0], load_sizes[0]); /* -------- copy the value and insert \0 so strcmp works properly */
                 value[load_sizes[0]] = '\0';
-                if (done_skipping) {
+                if (done_skipping) { /* ---------------------------------- since we are done skipping ahead by chunks, check every row for gte */
                     if (strcmp(value, match) >= 0) {
                         DUMP(0, load_max, load_columns, load_sizes, load_size);
                         matched = 1;
                     }
-                } else if (strcmp(value, match) < 0) {
+                } else if (strcmp(value, match) < 0) { /* ---------------- we aren't done skipping ahead, we wan't to keep skipping until we've gone too far */
                     READ_GOTO_NEXT_CHUNK(0);
-                } else {
+                } else { /* ---------------------------------------------- we've gone too far, time to backup one chunk and start checking every row */
                     READ_GOTO_LAST_CHUNK(0);
                     done_skipping = 1;
                 }
--- test/bdropuntil_test.py
+++ test/bdropuntil_test.py
@@ -2,7 +2,7 @@ import os
 import string
 import shell
 from hypothesis import given, settings
-from hypothesis.strategies import text, lists, composite, integers, randoms, sampled_from
+from hypothesis.strategies import text, lists, composite, integers, randoms, sampled_from, floats
 import random
 from test_util import run, compile_buffer_sizes
 
@@ -23,36 +23,55 @@ def teardown_module():
     with shell.climb_git_root():
         shell.run('make clean', stream=True)
 
-def partition(r, n, x):
-    res = []
-    ks = list(sorted({r.randint(1, max(1, len(x))) for _ in range(n)}))
-    ks = [0] + ks
-    ks[-1] = len(x)
-    for a, b in zip(ks, ks[1:]):
-        res.append(x[a:b])
-    return res
-
 @composite
-def inputs(draw):
-    buffer = draw(sampled_from(buffers))
+def buffer_inputs(draw): # correct errors for very small buffers is a bit complicated
     r = draw(randoms())
+    def partition(n, x):
+        res = []
+        ks = list(sorted({r.randint(1, max(1, len(x))) for _ in range(n)}))
+        ks = [0] + ks
+        ks[-1] = len(x)
+        for a, b in zip(ks, ks[1:]):
+            res.append(x[a:b])
+        return res
+    buffer = draw(sampled_from(buffers))
     bytes_available = buffer - 2
     max_columns = bytes_available // 2
     num_columns = draw(integers(min_value=1, max_value=min(64, max_columns)))
     bytes_available -= num_columns * 2
     line = text(string.ascii_lowercase, min_size=0, max_size=bytes_available)
     line = line.filter(lambda x: len(x) > 0)
-    line = line.map(lambda x: partition(r, num_columns, x))
+    line = line.map(lambda x: partition(num_columns, x))
     lines = draw(lists(line, min_size=1))
     first_column_values = [line[0] for line in lines]
     for line in lines:
-        if line and random.random() > 0.8:
-            line[0] = random.choice(first_column_values)
+        if line and r.random() > 0.8:
+            line[0] = r.choice(first_column_values)
     lines = sorted(lines)
     lines = '\n'.join([','.join(l)[:bytes_available + num_columns - 1] # swapping out line[0] sometimes causes oversized rows, so trim to max row size
                        for l in lines if l]).strip() + '\n'
-    value = random.choice(first_column_values)
+    value = r.choice(first_column_values)
     return buffer, value, lines
+
+with open('/usr/share/dict/words') as f:
+    words = f.read().splitlines()
+
+@composite
+def inputs(draw):
+    r = draw(randoms())
+    word = sampled_from(words)
+    num_columns = draw(integers(min_value=1, max_value=64))
+    line = lists(word, min_size=1, max_size=num_columns)
+    lines = draw(lists(line, min_size=1))
+    first_column_values = [line[0] for line in lines]
+    threshold = draw(floats(min_value=0, max_value=1))
+    for line in lines:
+        if line and r.random() > threshold:
+            line[0] = r.choice(first_column_values)
+    lines = sorted(lines)
+    csv = '\n'.join([','.join(l) for l in lines if l]).strip() + '\n'
+    value = r.choice(first_column_values + [r.choice(words) for _ in range(len(lines))]) # sometimes look for values that arent in the dataset
+    return value, csv
 
 def expected(value, csv):
     res = []
@@ -70,6 +89,13 @@ def expected(value, csv):
 @given(inputs())
 @settings(max_examples=100 * int(os.environ.get('TEST_FACTOR', 1)), deadline=os.environ.get("TEST_DEADLINE", 1000 * 60))
 def test_props(args):
+    value, csv = args
+    result = expected(value, csv)
+    assert result == run(csv, f'bin/bsv | bin/bdropuntil "{value}" | bin/csv')
+
+@given(buffer_inputs())
+@settings(max_examples=100 * int(os.environ.get('TEST_FACTOR', 1)), deadline=os.environ.get("TEST_DEADLINE", 1000 * 60))
+def test_props_buffers(args):
     buffer, value, csv = args
     result = expected(value, csv)
     assert result == run(csv, f'bin/bsv.{buffer} | bin/bdropuntil.{buffer} "{value}" | bin/csv.{buffer}')
