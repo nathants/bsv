a2ba03dc1eb429c8fd8359e990395e5191959f70
nathants <me@nathants.com>
2019-12-29 12:47:56 -0800

    named size

--- src/_copy.c
+++ src/_copy.c
@@ -3,7 +3,7 @@
 int main(int argc, const char **argv) {
     char *buff;
     int size = 1024*1024*5;
-    buff = malloc(1024*1024*5);
+    buff = malloc(size);
     int wbytes, rbytes;
     while (1) {
         rbytes = fread_unlocked(buff, 1, size, stdin);
