a3555aa4b881d80cbed00c8e4a5f19368d360f88
nathants <me@nathants.com>
2020-01-04 21:45:49 -0800

    check error codes from xxh3

--- src/xxh3.c
+++ src/xxh3.c
@@ -3,7 +3,6 @@
 #include "write_simple.h"
 #include "xxh3.h"
 
-
 #define NUM_ARGS 0
 #define DESCRIPTION "xxh3_64 hash stdin, defaults to hex, can be --int, or --stream to hex and pass stdin through\n\n"
 #define USAGE "... | xxh3 [--stream|--int]\n\n"
@@ -15,14 +14,14 @@ int main(int argc, const char **argv) {
     FILE *read_files[1] = {stdin};
     READ_INIT(read_files, 1);
     FILE *write_files[1] = {stdout};
-    WRITE_INIT(write_files, 1)
+    WRITE_INIT(write_files, 1);
     XXH3_state_t state;
-    XXH3_64bits_reset(&state);
+    ASSERT(XXH3_64bits_reset(&state) != XXH_ERROR, "xxh3 reset failed\n");
     int stream_mode = argc > 1 && strcmp(argv[1], "--stream") == 0;
     if (stream_mode) {
         while (1) {
             READ(BUFFER_SIZE, 0);
-            XXH3_64bits_update(&state, read_buffer, read_bytes);
+            ASSERT(XXH3_64bits_update(&state, read_buffer, read_bytes) != XXH_ERROR, "xxh3 update failed\n");
             WRITE(read_buffer, read_bytes, 0);
             if (BUFFER_SIZE != read_bytes)
                 break;
@@ -30,7 +29,7 @@ int main(int argc, const char **argv) {
     } else {
         while (1) {
             READ(BUFFER_SIZE, 0);
-            XXH3_64bits_update(&state, read_buffer, read_bytes);
+            ASSERT(XXH3_64bits_update(&state, read_buffer, read_bytes) != XXH_ERROR, "xxh3 update failed\n");
             if (BUFFER_SIZE != read_bytes)
                 break;
         }
