648f5a2effaaad34bb483cccedec6cad11cd1e91
nathants <me@nathants.com>
2020-03-09 13:28:55 -0700

    make read.h more explicit

--- util/read.h
+++ util/read.h
@@ -54,14 +54,16 @@
         ASSERT(r_bytes_left >= 0, "fatal: negative r_bytes_left: %d\n", r_bytes_left);                                                      \
         if (r_bytes_left == 0) { /* --------------------------------------------------------- time to read the next chunk */                \
             r_bytes = fread_unlocked(&r_chunk_size[i], 1, sizeof(int32_t), r_files[i]); /* -- read chunk header to get size of chunk */     \
-            if (r_bytes != sizeof(int32_t)) { /* -------------------------------------------- EOF so there is no next chunk */              \
-                ASSERT(!ferror(r_files[i]), "fatal: couldnt read input\n");                                                                 \
-                r_chunk_size[i] = r_offset[i] = read_bytes = 0;                                                                             \
-            } else {                                                                                                                        \
+            if (r_bytes == sizeof(int32_t)) { /* -------------------------------------------- EOF so there is no next chunk */              \
                 FREAD(r_buffer[i], r_chunk_size[i], r_files[i]); /* ------------------------- read the chunk body */                        \
                 r_offset[i] = 0; /* --------------------------------------------------------- start at the beggining of the new chunk */    \
                 r_bytes_left = r_chunk_size[i] - r_offset[i]; /* ---------------------------- bytes left in the new chunk */                \
                 ASSERT(size <= r_bytes_left, "fatal: diskread, shouldnt happen, chunk sizes are known\n");                                  \
+            } else if (r_bytes == 0) {                                                                                                      \
+                ASSERT(!ferror(r_files[i]), "fatal: couldnt read input\n");                                                                 \
+                r_chunk_size[i] = r_offset[i] = read_bytes = 0;                                                                             \
+            } else {                                                                                                                        \
+                ASSERT(0, "fatal: bytes read should always be either 0 or what was expected\n");                                            \
             }                                                                                                                               \
         } else {                                                                                                                            \
             ASSERT(size <= r_bytes_left, "fatal: ramread, shouldnt happen, chunk sizes are known\n");                                       \
