87d2b6aec0e93e0c7d9a72a74fdb908ade271b5d
nathants <me@nathants.com>
2020-03-08 18:10:30 -0700

    docs

--- util/dump.h
+++ util/dump.h
@@ -10,19 +10,20 @@
 #define DUMP(i, max, columns, types, sizes, size)                                                                                                                                       \
     do {                                                                                                                                                                                \
         ASSERT(max <= MAX_COLUMNS, "fatal: cannot have more then 2**16 columns\n");                                                                                                     \
-        WRITE_START(sizeof(uint16_t) +                                                                                                                                                  \
-                    ((max + 1) * sizeof(uint8_t)) +                                                                                                                                     \
-                    ((max + 1) * sizeof(uint16_t)) +                                                                                                                                    \
-                    size * sizeof(uint8_t), i);                                                                                                                                         \
-        WRITE(INT32_TO_UINT16(max), sizeof(uint16_t), i);                                                                                                                               \
+        /* -------------------------------------------------------------- write start in case total size of writes would flush the buffer we want to flush it immediately */             \
+        WRITE_START(sizeof(uint16_t) + /* ------------------------------- max */                                                                                                         \
+                    ((max + 1) * sizeof(uint8_t)) +  /* ----------------- types */                                                                                                       \
+                    ((max + 1) * sizeof(uint16_t)) + /* ----------------- sizes */                                                                                                       \
+                    size * sizeof(uint8_t), i); /* ---------------------- buffer */                                                                                                      \
+        WRITE(INT32_TO_UINT16(max), sizeof(uint16_t), i); /* ------------ write max */                                                                                                               \
         for (d_i = 0; d_i <= max; d_i++)                                                                                                                                                \
-            WRITE(INT32_TO_UINT8(types[d_i]), sizeof(uint8_t), i);                                                                                                                        \
+            WRITE(INT32_TO_UINT8(types[d_i]), sizeof(uint8_t), i); /* --- write types */                                                                                                 \
         for (d_i = 0; d_i <= max; d_i++) {                                                                                                                                              \
             ASSERT(sizes[d_i] <= MAX_COLUMNS, "fatal: cannot have columns with more than 2**16 bytes, column: %d, size: %d, content: %.*s...\n", d_i, sizes[d_i], 10, columns[d_i]);    \
-            WRITE(INT32_TO_UINT16(sizes[d_i]), sizeof(uint16_t), i);                                                                                                                    \
+            WRITE(INT32_TO_UINT16(sizes[d_i]), sizeof(uint16_t), i); /* - write sizes */                                                                                                \
         }                                                                                                                                                                               \
         for (d_i = 0; d_i <= max; d_i++)                                                                                                                                                \
-            WRITE(columns[d_i], sizes[d_i], i);                                                                                                                                         \
+            WRITE(columns[d_i], sizes[d_i], i); /* ---------------------- write buffer */                                                                                               \
     } while(0)
 
 #define DUMP_FLUSH(i) WRITE_FLUSH(i);
