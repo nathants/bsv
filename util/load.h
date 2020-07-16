20994b4addf59fdb19bd84c923ba25e84f5fe49c
nathants <me@nathants.com>
2020-07-16 16:12:08 -0700

    readme

--- util/load.h
+++ util/load.h
@@ -10,7 +10,7 @@ inlined void load_next(readbuf_t *rbuf, row_t *row, i32 file) {
             row->stop = 0;
             row->max = FROM_UINT16(rbuf->buffer); // ---------------------------- parse max
             read_bytes_assert(rbuf, (row->max + 1) * sizeof(u16), file); // ----- read sizes
-            i32 size = row->max + 1; // ----------------------------------------- total size in bytes of all columns, including trailing \0's
+            i32 size = row->max + 1; // ----------------------------------------- total size in bytes of all columns, including trailing \0
             for (i32 i = 0; i <= row->max; i++) {
                 row->sizes[i] = FROM_UINT16(rbuf->buffer + i * sizeof(u16)); // - parse sizes
                 size += row->sizes[i]; // --------------------------------------- update total size
@@ -18,7 +18,7 @@ inlined void load_next(readbuf_t *rbuf, row_t *row, i32 file) {
             read_bytes_assert(rbuf, size * sizeof(u8), file); // ---------------- row all column bytes
             row->columns[0] = rbuf->buffer;
             for (i32 i = 0; i < row->max; i++)
-                row->columns[i + 1] = row->columns[i] + row->sizes[i] + 1; // --- setup zerocopy pointers to read_buffer and skip trailing \0 values
+                row->columns[i + 1] = row->columns[i] + row->sizes[i] + 1; // --- setup pointers to read_buffer and skip trailing \0
             break;
         case 0:
             row->stop = 1; // --------------------------------------------------- empty read means EOF
