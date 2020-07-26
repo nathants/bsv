4e65ab915a68eeaecc09511648cce16c77532898
nathants <me@nathants.com>
2020-07-25 17:10:46 -0700

    install script needs gcc and make

--- scripts/install_archlinux.sh
+++ scripts/install_archlinux.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -euo pipefail
-tempdir=$(mktemp -d)
-trap "cd /tmp && rm -rf $tempdir" EXIT
-cd $tempdir
+sudo pacman --needed --noconfirm --noprogressbar -Sy \
+     gcc \
+     make
+cd ~
+rm -rf bsv
 git clone https://github.com/nathants/bsv
 cd bsv
 make -j
