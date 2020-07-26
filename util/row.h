b47f5825eec535a195e7a98a2d2a6a44e70d151c
nathants <me@nathants.com>
2020-07-26 13:58:29 -0700

    functions for a raw row which mallocs and frees the two internal buffers

--- util/row.h
+++ util/row.h
@@ -25,3 +25,19 @@ inlined void row_to_raw(row_t *row, raw_row_t *raw_row) {
     for (i32 i = 0; i <= row->max; i++)
         raw_row->buffer_size += row->sizes[i] + 1;
 }
+
+inlined void row_to_raw_malloc(row_t *row, raw_row_t *raw_row) {
+    raw_row->header_size = sizeof(u16) + (row->max + 1) * sizeof(u16);
+    MALLOC(raw_row->header, raw_row->header_size);
+    memcpy(raw_row->header, row->columns[0] - raw_row->header_size, raw_row->header_size);
+    raw_row->buffer_size = 0;
+    for (i32 i = 0; i <= row->max; i++)
+        raw_row->buffer_size += row->sizes[i] + 1;
+    MALLOC(raw_row->buffer, raw_row->buffer_size);
+    memcpy(raw_row->buffer, row->columns[0], raw_row->buffer_size);
+}
+
+inlined void raw_row_free(raw_row_t *raw_row) {
+    free(raw_row->header);
+    free(raw_row->buffer);
+}
